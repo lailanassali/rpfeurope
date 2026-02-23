a4a1ebe4af832b3c55bed82225410d7ee9dbf63f
braingrams <braingrams40@gmail.com>
2026-02-23 16:53:56 +0000

    fix issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,29 +3,55 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 const montserrat = Montserrat({
- subsets: ["latin"],
- variable: "--font-montserrat",
+	subsets: ["latin"],
+	variable: "--font-montserrat",
 });
 
 export const metadata: Metadata = {
- title: "Redeemed Pillar of Fire - RPF Europe",
- description: "Welcome to Redeemed Pillar of Fire Europe. A place of worship, community, and spiritual growth.",
- keywords: ["Church", "Ministry", "RPF", "Europe", "Redeemed Pillar of Fire", "Worship", "Community"],
- icons: {
-  icon: '/assets/rpflogo.png',
- },
+	// metadataBase: new URL("https://rpfeurope.org"),
+	title: "Redeemed Pillar of Fire - RPF Europe",
+	description:
+		"Welcome to Redeemed Pillar of Fire Europe. A place of worship, community, and spiritual growth.",
+	keywords: [
+		"Church",
+		"Ministry",
+		"RPF",
+		"Europe",
+		"Redeemed Pillar of Fire",
+		"Worship",
+		"Community",
+	],
+	openGraph: {
+		title: "Redeemed Pillar of Fire - RPF Europe",
+		description:
+			"Welcome to Redeemed Pillar of Fire Europe. A place of worship, community, and spiritual growth.",
+		images: ["/assets/rpflogo.png"],
+	},
+	twitter: {
+		card: "summary",
+		title: "Redeemed Pillar of Fire - RPF Europe",
+		description:
+			"Welcome to Redeemed Pillar of Fire Europe. A place of worship, community, and spiritual growth.",
+		images: ["/assets/rpflogo.png"],
+	},
+	icons: {
+		icon: "/assets/rpflogo.png",
+	},
 };
 
 export default function RootLayout({
- children,
+	children,
 }: Readonly<{
- children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
- return (
-  <html lang="en">
-   <body className={`${montserrat.className}`} style={{ maxWidth: '1800px', margin: '0 auto' }}>
-    {children}
-   </body>
-  </html>
- );
+	return (
+		<html lang="en">
+			<body
+				className={`${montserrat.className}`}
+				style={{ maxWidth: "1800px", margin: "0 auto" }}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
